eba887c0c0aae6b42ef57b609e2a3061c62d598f
angge28 <angelicagucela28@gmail.com>
2022-11-02 11:37:51 +0800

    model-only

--- src/main/java/org/group4/revalida/onlineFarmingAssistant/model/farmer/Complaints.java
+++ src/main/java/org/group4/revalida/onlineFarmingAssistant/model/farmer/Complaints.java
@@ -1,5 +1,78 @@
 package org.group4.revalida.onlineFarmingAssistant.model.farmer;
 
-public class Complaints {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.group4.revalida.onlineFarmingAssistant.model.shared.Account;
+
+@Entity
+@Table(name="complaints_table")
+public class Complaints {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long complaintId;
+	private Account account;
+	private String complaintPost;
+	private String complaintImg;
+	private boolean isRead;
+	
+	public Complaints() {
+		
+	}
+	
+	public Complaints(Long complaintId, Account account, String complaintPost, String complaintImg, boolean isRead) {
+		super();
+		this.complaintId = complaintId;
+		this.account = account;
+		this.complaintPost = complaintPost;
+		this.complaintImg = complaintImg;
+		this.isRead = isRead;
+	}
+
+	public Long getComplaintId() {
+		return complaintId;
+	}
+
+	public void setComplaintId(Long complaintId) {
+		this.complaintId = complaintId;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public String getComplaintPost() {
+		return complaintPost;
+	}
+
+	public void setComplaintPost(String complaintPost) {
+		this.complaintPost = complaintPost;
+	}
+
+	public String getComplaintImg() {
+		return complaintImg;
+	}
+
+	public void setComplaintImg(String complaintImg) {
+		this.complaintImg = complaintImg;
+	}
+
+	public boolean isRead() {
+		return isRead;
+	}
+
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
+	
+	
+	
 }
---
+++ src/main/java/org/group4/revalida/onlineFarmingAssistant/repo/farmer/BidRepo.java
@@ -0,0 +1,8 @@
+package org.group4.revalida.onlineFarmingAssistant.repo.farmer;
+
+import org.group4.revalida.onlineFarmingAssistant.model.farmer.Bid;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BidRepo extends JpaRepository<Bid, Long> {
+
+}
