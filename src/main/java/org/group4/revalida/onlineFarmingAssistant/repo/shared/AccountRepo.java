af795332b493e4a967ed87007593b0bb58c6b35d
GroupFour77 <sevenseven403@yahoo.com>
2022-11-05 13:12:08 +0800

    Spring security done

--- src/main/java/org/group4/revalida/onlineFarmingAssistant/repo/shared/AccountRepo.java
+++ src/main/java/org/group4/revalida/onlineFarmingAssistant/repo/shared/AccountRepo.java
@@ -4,5 +4,5 @@ import org.group4.revalida.onlineFarmingAssistant.model.shared.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepo extends JpaRepository<Account, Long>{
-
+Account findByUsername(String username);
 }
