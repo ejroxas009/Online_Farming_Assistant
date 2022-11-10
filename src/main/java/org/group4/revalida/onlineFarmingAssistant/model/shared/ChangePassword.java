89cae318ff9dadb373e23509d186ba31406cf6b5
GroupFour77 <sevenseven403@yahoo.com>
2022-11-10 23:38:25 +0800

    added changepw and changeprofileImg endpoints

---
+++ src/main/java/org/group4/revalida/onlineFarmingAssistant/model/shared/ChangePassword.java
@@ -0,0 +1,20 @@
+package org.group4.revalida.onlineFarmingAssistant.model.shared;
+
+public class ChangePassword {
+	private String oldPassword;
+	private String newPassword;
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	
+	
+}
